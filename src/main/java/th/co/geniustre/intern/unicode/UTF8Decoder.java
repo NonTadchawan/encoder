893388f09193b08c6cbee23422d16ea3e958b99b
non <tadchawan@geniustree.co.th>
2023-06-18 12:48:25 +0700

    remove Integer[] ints={}

--- src/main/java/th/co/geniustre/intern/unicode/UTF8Decoder.java
+++ src/main/java/th/co/geniustre/intern/unicode/UTF8Decoder.java
@@ -7,25 +7,22 @@ public class UTF8Decoder {
         StringBuilder sb = new StringBuilder();
         final Integer test1 = 0b1110_0000;
         final Integer test2 = 0b1000_0000;
-        Integer[] ints = {};
         Integer result = 0;
         Integer prev = 0;
 
         for (int i = 0; i < utf8s.length; i++) {
             if (test1.equals(test1 & utf8s[i])) {
-                ints = new Integer[utf8s.length];
                 prev = utf8s[i] & (0b0000_1111);
-                ints[i] = prev << 12;
+                result = result + (prev << 12);
             } else if (test2.equals(test2 & utf8s[i])) {
                 int i1 = utf8s[i] & (0b0011_1111);
-                if (prev < 15) {
-                    ints[i] = i1 << 6;
+                if (prev <= 15) {
+                    result = result +(i1 << 6);
                 } else {
-                    ints[i] = i1;
+                    result = result + i1;
                 }
                 prev = i1;
             }
-            result = result + ints[i];
             if ((i + 1) % 3 == 0) {
                 String st = unicode.getUnicodeChars().get(result).toString();
                 sb.append(st);
